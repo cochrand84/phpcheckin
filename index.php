1873c76e337d599aae8193edae94a3939e75008b
cochrand84 <david@davidcochran.us>
2018-10-16 16:36:19 -0500

    adding css and more photos
    adding css and more photos

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Index PHPCheckin 0.01</title>
+<title>Index PHPCheckin 0.02</title>
 <!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
 <!-- InstanceEndEditable -->
@@ -12,6 +12,9 @@
 
 <body>
 <!-- InstanceBeginEditable name="Form" -->
+	
+<input type="text" name="current_date" value="<?php echo date('H:i:s M d, Y'); ?>" readonly="readonly">
+	
 Name: <input type="text" name="name" value="<?php echo $name;?>">
 	</p>
 E-mail: <input type="text" name="email" value="<?php echo $email;?>">
@@ -19,7 +22,22 @@ E-mail: <input type="text" name="email" value="<?php echo $email;?>">
 Bike Vin #: <input type="text" name="vin" value="<?php echo $vin;?>">
 	</p>
 <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
+    Select clutch side image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</p>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select brake side image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</p>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select front image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</p>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select back image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </p>
