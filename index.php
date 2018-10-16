d8bc1d49e933448ad016820c9f14780817c04d7b
cochrand84 <david@davidcochran.us>
2018-10-16 16:23:50 -0500

    a little cleanup and some style
    a little cleanup and some style

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html><!-- InstanceBegin template="/Templates/form1.dwt" codeOutsideHTMLIsLocked="false" -->
+<html><!-- InstanceBegin template="/Templates/tmp1.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta charset="utf-8">
 <!-- InstanceBeginEditable name="doctitle" -->
@@ -13,11 +13,16 @@
 <body>
 <!-- InstanceBeginEditable name="Form" -->
 Name: <input type="text" name="name" value="<?php echo $name;?>">
-
+	</p>
 E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-
+</p>
 Bike Vin #: <input type="text" name="vin" value="<?php echo $vin;?>">
-
+	</p>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</p>
 Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
 <!-- InstanceEndEditable -->
 </body>
