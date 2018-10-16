05df4afa4f12ce1e936f317e4e9f78a83f9bfc57
cochrand84 <david@davidcochran.us>
2018-10-16 16:19:14 -0500

    initial building of the form
    initial building of the form... or at least the start of a form

--- index.php
+++ index.php
@@ -3,12 +3,22 @@
 <head>
 <meta charset="utf-8">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Untitled Document</title>
+<title>Index PHPCheckin 0.01</title>
 <!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
 <!-- InstanceEndEditable -->
+
 </head>
 
 <body>
+<!-- InstanceBeginEditable name="Form" -->
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+
+Bike Vin #: <input type="text" name="vin" value="<?php echo $vin;?>">
+
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+<!-- InstanceEndEditable -->
 </body>
 <!-- InstanceEnd --></html>
