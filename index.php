754eff9a42c20642e7128d88cb71d87ce92badff
cochrand84 <david@davidcochran.us>
2018-10-16 16:52:05 -0500

    adding ver and some css changes
    like adding the css include tag...lol

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!doctype html>
 <html><!-- InstanceBegin template="/Templates/tmp1.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
+	<link rel="stylesheet" href="main.css">
+	<?php $ver = ".03";?>
 <meta charset="utf-8">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Index PHPCheckin 0.02</title>
+<title>Index PHPCheckin <?php echo $ver;?></title>
 <!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
 <!-- InstanceEndEditable -->
@@ -41,7 +43,12 @@ Bike Vin #: <input type="text" name="vin" value="<?php echo $vin;?>">
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </p>
+<div class="row">
+      <input type="submit" value="Submit">
+</div>
 Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
 <!-- InstanceEndEditable -->
+</p>
+<?php echo $ver;?>
 </body>
 <!-- InstanceEnd --></html>
