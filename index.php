dda6d083da6f88ed6cff75101af77f496121926e
cochrand84 <david@davidcochran.us>
2018-10-16 15:55:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!doctype html>
-<html>
+<html><!-- InstanceBegin template="/Templates/form1.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
 <title>Untitled Document</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
 </head>
 
 <body>
 </body>
-</html>
+<!-- InstanceEnd --></html>
