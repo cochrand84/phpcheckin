c14c3e681b8ac55f755b5b56349cebde5c6f8ec5
cochrand84 <david@davidcochran.us>
2018-10-17 15:59:57 -0500

    Update includes/upload.php

--- includes/upload.php
+++ includes/upload.php
@@ -2,7 +2,7 @@
 
 include "connect.php";
 
-$sql="INSERT INTO tickets (firstname, lastname) VALUES ('$_POST[firstname]','$_POST[lastname]')";
+$sql="INSERT INTO tickets (firstname, lastname, check_in_date) VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[check_in_date]')";
 
 if (!mysqli_query($con,$sql))
 
