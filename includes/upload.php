c8e35c51aeafd758c371343cebddd78e04d9819c
cochrand84 <david@davidcochran.us>
2018-10-17 17:17:21 -0500

    image upload to mysql take 2

--- includes/upload.php
+++ includes/upload.php
@@ -2,7 +2,7 @@
 
 include "connect.php";
 
-$sql="INSERT INTO tickets (firstname, lastname, check_in_date, due_date, status) VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[check_in_date]','$_POST[due_date]','$_POST[status]')";
+$sql="INSERT INTO tickets (firstname, lastname, check_in_date, due_date, status, image1) VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[check_in_date]','$_POST[due_date]','$_POST[status]','$_POST[image1]')";
 
 if (!mysqli_query($con,$sql))
 
--- index.php
+++ index.php
@@ -58,25 +58,25 @@
 		Select image to upload: 
 	</div>
 	<div class="col-75">
-	  <input type="file" name="image">
+	  <input type="file" name="image1">
     </div>
 	<div class="col-25">
 		Select image to upload: 
 	</div>
 	<div class="col-75">
-	  <input type="file" name="image">
+	  <input type="file" name="image2">
     </div>
 	  <div class="col-25">
 		Select image to upload: 
 	</div>
 	<div class="col-75">
-	  <input type="file" name="image">
+	  <input type="file" name="image3">
     </div>
 	  <div class="col-25">
 		Select image to upload: 
 	</div>
 	<div class="col-75">
-	  <input type="file" name="image">
+	  <input type="file" name="image4">
     </div>
 	
 	<div class="col-25">
