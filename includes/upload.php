b2ae084ca76b32c9b288284de89aea1aaade076c
cochrand84 <david@davidcochran.us>
2018-10-16 21:25:53 -0500

    Update includes/upload.php

--- includes/upload.php
+++ includes/upload.php
@@ -10,4 +10,15 @@ if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
 } else {
     echo "Image uploading failed.";
 }
+
+$sql = "INSERT INTO tickets (record_number,firstname, lastname, email)
+VALUES ('$_POST['record_number']','$_POST['firstname']', '$_POST['lastname']', '$_POST['email']')";
+
+if ($connection->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$connection->close();
 ?> 
