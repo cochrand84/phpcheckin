0d69bcc36690ac7bb885a008896982d8fac2abd9
cochrand84 <david@davidcochran.us>
2018-10-16 22:10:29 -0500

    more stuff trying to figure out sql

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,11 @@
 <?php
 require_once("config.php");
-$connection = mysqli_connect($mysql_database_server, $mysql_database_username, $mysql_database_password);
-if (!$connection){
-    die("Database Connection Failed" . mysqli_error($connection));
+$con = mysql_connect($mysql_database_server, $mysql_database_username, $mysql_database_password);
+if (!$con){
+    die("Database Connection Failed" . mysql_error($con));
 }
-$select_db = mysqli_select_db($connection, $mysql_databasename);
+$select_db = mysql_select_db($con, $mysql_databasename);
 if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($connection));
+    die("Database Selection Failed" . mysql_error($con));
 }
 ?>
--- includes/upload.php
+++ includes/upload.php
@@ -1,25 +1,37 @@
 <?php
 include 'config.php';
+include 'connect.php';
 
-$uploaddir = 'uploads/';
+if (!$con)
 
-$uploadfile = $uploaddir . basename($_FILES['image']['name']);
+  {
 
-//if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
-//    echo "Image succesfully uploaded.";
-//} else {
-//    echo "Image uploading failed.";
-//}
+  die('Could not connect: ' . mysql_error());
 
+  }
 
 
-  if(isset($_POST['save']))
-{
-    $sql = "INSERT INTO tickets (record_number, firstname, lastname)
-    VALUES ('".$_POST["record_number"]."','".$_POST["firstname"]."','".$_POST["lastname"]."')";
+ 
 
-    $result = mysqli_query($connection,$sql);
-}
-echo $result;
-$connection->close();
+$sql="INSERT INTO tickets (record_number, lastname)
+
+VALUES
+
+('$_POST[record_number]','$_POST[lastname]')";
+
+ 
+
+if (!mysql_query($sql,$con))
+
+  {
+
+  die('Error: ' . mysql_error());
+
+  }
+
+echo "1 record added";
+
+ 
+
+mysql_close($con)
 ?> 
