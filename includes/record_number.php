ee50006b1749570bcb8fb5ed7504272c62cc114b
cochrand84 <david@davidcochran.us>
2018-10-16 20:41:26 -0500

    Update includes/record_number.php

--- includes/record_number.php
+++ includes/record_number.php
@@ -1,9 +1,5 @@
 <?php
 require_once("config.php");
     $current_record_number = mysqli_query('SELECT record_number FROM tickets ORDER BY  record_number DESC');
-    echo 'current record number=';
-    echo $current_record_number;
     $new_record_number = $current_record_number + 1;
-    echo '   new record number=';
-    echo $new_record_number;
 ?>
