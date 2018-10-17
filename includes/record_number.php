370870d8baa3b40059d1a859fccc2876922b93df
cochrand84 <david@davidcochran.us>
2018-10-17 14:43:50 -0500

    Update includes/record_number.php

--- includes/record_number.php
+++ includes/record_number.php
@@ -1,9 +1,6 @@
 <?php
 include "connect.php";
 	$record_sql = "SELECT MAX(record_number) FROM tickets";
-	echo $record_sql;
-    $current_record_number = mysqli_query($con, $record_sql);
-    echo $current_record_number;
+	$current_record_number = mysqli_query($con, $record_sql);
     $new_record_number = $current_record_number + 1;
-    echo $new_record_number;
-?>
+    ?>
