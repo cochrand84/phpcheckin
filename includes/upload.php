3affef996b6facfbd17374f232a490f9b6674656
cochrand84 <david@davidcochran.us>
2018-10-18 17:56:03 -0500

    trying to fix forms and mysql

--- includes/upload.php
+++ includes/upload.php
@@ -2,33 +2,6 @@
 
 include "connect.php";
 
-$name= $_FILES['image1']['name'];
-
-$tmp_name= $_FILES['image1']['tmp_name'];
-
-$submitbutton= $_POST['submit'];
-
-$position= strpos($name, "."); 
-
-$fileextension= substr($name, $position + 1);
-
-$fileextension= strtolower($fileextension);
-
-$description= $_POST['description_entered'];
-
-if (isset($name)) {
-
-$path= 'uploads/';
-
-if (!empty($name)){
-if (move_uploaded_file($tmp_name, $path.$name)) {
-echo 'Uploaded!';
-
-}
-}
-}
-
-
 
 $sql="INSERT INTO tickets (firstname, lastname, check_in_date, due_date, status, image1) VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[check_in_date]','$_POST[due_date]','$_POST[status]','$_FILES["$name"]')";
 
--- index.php
+++ index.php
@@ -90,5 +90,63 @@ Comment:
 	</div>
 	  </form>
 <?php include "includes/footer.php"; ?>
+
+<?php
+  // Create database connection
+  include "includes/connect.php";
+  $db = $con;
+
+  // Initialize message variable
+  $msg = "";
+
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
+
+  	// image file directory
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM images");
+?>
+<div id="content">
+  <?php
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div id='img_div'>";
+      	echo "<img src='images/".$row['image']."' >";
+      	echo "<p>".$row['image_text']."</p>";
+      echo "</div>";
+    }
+  ?>
+  <form method="POST" action="index.php" enctype="multipart/form-data">
+  	<input type="hidden" name="size" value="1000000">
+  	<div>
+  	  <input type="file" name="image">
+  	</div>
+  	<div>
+      <textarea 
+      	id="text" 
+      	cols="40" 
+      	rows="4" 
+      	name="image_text" 
+      	placeholder="Say something about this image..."></textarea>
+  	</div>
+  	<div>
+  		<button type="submit" name="upload">POST</button>
+  	</div>
+  </form>
+</div>
 </body>
 </html>
