28cc4e4948224add2bd0431a7a0a1d1c414d7275
cochrand84 <david@davidcochran.us>
2018-12-10 17:06:39 -0600

    editing read
    editing read

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php include "templates/header.php"; ?>
 
 <ul>
-	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+	<li><a href="create.php"><strong>Create</strong></a> - add a new ticket</li>
+	<li><a href="read.php"><strong>Read</strong></a> - show tickets</li>
 </ul>
 
 <?php include "templates/footer.php"; ?>
