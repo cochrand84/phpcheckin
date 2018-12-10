e7a9b7471ba42c02ff553aca11b04263dcdf95f8
cochrand84 <david@davidcochran.us>
2018-12-10 16:20:57 -0600

    chane

--- includes/config-default.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_databasename = 'php_checkin';
-$mysql_database_username = 'php_checkin';
-$mysql_database_password = 'password';
-$mysql_database_server = 'localhost';
-
-$ver = '0.6.5'
-?>
