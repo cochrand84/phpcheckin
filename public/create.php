db716fbe395c01c57bf3842d2b4c2fdf9b08d615
cochrand84 <david@davidcochran.us>
2018-12-10 19:00:16 -0600

    Update create.php

--- public/create.php
+++ public/create.php
@@ -58,7 +58,7 @@ if (isset($_POST['submit'])) {
     <label for="location">Location</label>
     <input type="text" name="location" id="location">
     <label for="status">Status</label>
-    <select name="status" value="<?php echo escape($row["status"]); ?>">
+    <select name="status">
     <option value="selectstatus" selected>Select Status</option>
     <option value="checked-in">Checked-In</option>
     <option value="waiting">Waiting</option>
