d00ba3f4dbb5331a076a4fa3cae8df276d8a35e4
cochrand84 <david@davidcochran.us>
2018-12-12 17:59:13 -0600

    Update view.php

--- public/view.php
+++ public/view.php
@@ -1,4 +1,6 @@
 <?php
+
+require "../config.php";
 if(!empty($_GET['rand'])){
  
     $db = new mysqli($host, $username, $password, $dbname);
@@ -7,13 +9,11 @@ if(!empty($_GET['rand'])){
        die("Connection failed: " . $db->connect_error);
     }
     
-
     $result = $db->query("SELECT image1 FROM images WHERE rand = {$_GET['rand']}");
     
     if($result->num_rows > 0){
         $imgData = $result->fetch_assoc();
         
-
         header("Content-type: image/jpg"); 
         echo $imgData['image1'];
     }else{
