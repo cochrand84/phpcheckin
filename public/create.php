d763368d8fbd322b8b85ca9c919fea51748eccac
cochrand84 <david@davidcochran.us>
2018-12-12 16:51:17 -0600

    Update create.php

--- public/create.php
+++ public/create.php
@@ -167,8 +167,7 @@ if (isset($_POST['submit'])) {
             <option value="Ready For Pickup">Ready For Pickup</option>
     </select>
     <label for="image1">Image 1</label>
-    <input type="file" name="image1" id="image1" value="<?php echo $rand; ?>"/><input type="submit" name="upload" value="Upload"><br />
-    <input type="text" name="image1rand" id="image1rand" value="<?php echo $rand; ?>"/><br />
+    <input type="file" name="image1" id="image1" value="<?php echo $rand; ?>"/><br />
     <br><br>
     <input type="submit" name="submit" value="Submit">
 </form>
