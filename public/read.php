154cabf66013a4dfa50a916a22178f4c7d5a4c0c
cochrand84 <david@davidcochran.us>
2018-12-13 14:27:15 -0600

    adding new fields

--- public/read.php
+++ public/read.php
@@ -110,12 +110,49 @@ if (isset($_POST['submit3'])) {
     <?php } 
 } ?> 
 
+<?php
+if (isset($_POST['submit4'])) {
+    try  {
+        
+        require "../config.php";
+        require "../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT *  FROM tickets ORDER by due_date ASC";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+        
+<?php  
+if (isset($_POST['submit4'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+<?php require "templates/readalltable.php"; ?>
+
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
+    <?php } 
+} ?> 
+
 <h2>Select your view option</h2>
 
 <form method="post">
     <input type="submit" name="submit" value="View non-Complete">
     <input type="submit" name="submit2" value="View all">
     <input type="submit" name="submit3" value="View Complete">
+    <input type="submit" name="submit4" value="View all data">
 </form>
 
 <?php require "templates/footer.php"; ?>
---
+++ public/templates/readalltable.php
@@ -0,0 +1,70 @@
+               <table id="ticketstable">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Vin Number</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>email</th>
+                    <th>phone</th>
+                    <th>Year</th>
+                    <th>Date</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Due Date</th>
+                    <th>Location</th>
+                    <th>Status</th>
+                    <th>description</th>
+                    <th>oilchange</th>
+                    <th>fullservice</th>
+                    <th>otherservice</th>
+                    <th>otherservicedescription</th>
+                    <th>fronttirechange</th>
+                    <th>reartirechange</th>
+                    <th>audiotroubleshooting</th>
+                    <th>otheraudiodescription</th>
+                    <th>fullaudiosystem</th>
+                    <th>audioupgrade</th>
+                    <th>Image 1</th>
+                    <th>Image 2</th>
+                    <th>Image 3</th>
+                    <th>Image 4</th>
+                    <th>View Details</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php foreach ($result as $row) { ?>
+            <tr>
+                <td><?php echo escape($row["id"]); ?></td>
+                <td><?php echo escape($row["vin"]); ?> </td>
+                <td><?php echo escape($row["firstname"]); ?></td>
+                <td><?php echo escape($row["lastname"]); ?></td>
+                <td><?php echo excape($row["email"]); ?></td>
+                <td><?php echo excape($row["phone"]); ?></td>
+                <td><?php echo escape($row["year"]); ?></td>
+                <td><?php echo escape($row["date"]); ?> </td>
+                <td><?php echo escape($row["make"]); ?> </td>
+                <td><?php echo escape($row["model"]); ?> </td>
+                <td><?php echo escape($row["due_date"]); ?> </td>
+                <td><?php echo escape($row["location"]); ?></td>
+                <td><?php echo escape($row["status"]); ?> </td> 
+                <td><?php echo excape($row["description"]); ?></td>
+                <td><?php echo excape($row["oilchange"]); ?></td>
+                <td><?php echo excape($row["fullservice"]); ?></td>
+                <td><?php echo excape($row["otherservice"]); ?></td>
+                <td><?php echo excape($row["otherservicedescription"]); ?></td>
+                <td><?php echo excape($row["fronttirechange"]); ?></td>
+                <td><?php echo excape($row["reartirechange"]); ?></td>
+                <td><?php echo excape($row["audiotroubleshooting"]); ?></td>
+                <td><?php echo excape($row["otheraudiodescription"]); ?></td>
+                <td><?php echo excape($row["fullaudiosystem"]); ?></td>
+                <td><?php echo excape($row["audioupgrade"]); ?></td>         
+              <td><a href="view.php?rand=<?php echo $row["image1"]; ?>"><img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42"></a></td>
+              <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
+              <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
+              <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
+              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
