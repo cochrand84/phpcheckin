b88d70bb393aa1368d4a2d32937830958792a98d
cochrand84 <david@davidcochran.us>
2018-12-13 16:47:02 -0600

    Create display.php

---
+++ public/display.php
@@ -0,0 +1,47 @@
+<?php
+$page = $_SERVER['PHP_SELF'];
+$sec = "10";
+?>
+<html>
+    <head>
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+    </head>
+    <body>
+<?php
+require "templates/header.php";
+if (isset($_POST['submit'])) {
+    try  {
+        
+        require "../config.php";
+        require "../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * FROM tickets WHERE NOT (status = 'complete') ORDER by due_date ASC";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
+        
+<?php  
+if (isset($_POST['submit'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+<?php require "templates/readtable.php"; ?>
+
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
+    <?php } 
+} 
+?>
