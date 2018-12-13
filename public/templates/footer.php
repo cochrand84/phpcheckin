0c08b6f64ee9d411b0bf496fa1985ef18fc62337
cochrand84 <david@davidcochran.us>
2018-12-12 20:51:16 -0600

    styling buttons

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -1,4 +1,4 @@
 </body>
-<a href="templates/password_protect.php?logout=1">Logout</a>
-<a href="index.php">Back to home</a>
+<a href="templates/password_protect.php?logout=1" class="linkbutton">Logout</a>
+<a href="index.php" class="linkbutton">Back to home</a>
 </html>
