8041bc9e0dcbff3da0dcebef28bb72f59086fca5
cochrand84 <david@davidcochran.us>
2018-12-12 20:54:29 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 
 
 <ul>
-	<li><a href="create.php"><strong>Create</strong></a> - add a new ticket</li>
-	<li><a href="read.php"><strong>Read</strong></a> - show tickets</li>
-	<li><a href="edit.php"><strong>Edit</strong></a> - edit tickets</li>
+	<li><a href="create.php" class="linkbutton"><strong>Create</strong></a>add a new ticket</li>
+	<li><a href="read.php" class="linkbutton"><strong>Read</strong></a>show tickets</li>
+	<li><a href="edit.php" class="linkbutton"><strong>Edit</strong></a>edit tickets</li>
 </ul>
 
 <?php include "templates/footer.php"; ?>
