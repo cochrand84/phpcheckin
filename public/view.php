eda8eda3d2c877445b17e074b08b33b17c1af161
cochrand84 <david@davidcochran.us>
2018-12-12 18:01:50 -0600

    Update view.php

--- public/view.php
+++ public/view.php
@@ -1,19 +1,26 @@
 <?php
-
-require "../config.php";
 if(!empty($_GET['rand'])){
- 
-    $db = new mysqli($host, $username, $password, $dbname);
+    //DB details
+    $dbHost     = 'localhost';
+    $dbUsername = 'a51checkin';
+    $dbPassword = 'ylKy724$';
+    $dbName     = 'area51_checkin';
     
+    //Create connection and select DB
+    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    
+    //Check connection
     if($db->connect_error){
        die("Connection failed: " . $db->connect_error);
     }
     
+    //Get image data from database
     $result = $db->query("SELECT image1 FROM images WHERE rand = {$_GET['rand']}");
     
     if($result->num_rows > 0){
         $imgData = $result->fetch_assoc();
         
+        //Render image
         header("Content-type: image/jpg"); 
         echo $imgData['image1'];
     }else{
