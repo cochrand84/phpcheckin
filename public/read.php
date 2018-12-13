68c00cd2f770a006e31a721bcdbc42d298a1efe7
cochrand84 <david@davidcochran.us>
2018-12-12 22:20:45 -0600

    Update read.php

--- public/read.php
+++ public/read.php
@@ -29,49 +29,8 @@ if (isset($_POST['submit'])) {
     if ($result && $statement->rowCount() > 0) { ?>
         <h2>Results</h2>
 
-               <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Year</th>
-                    <th>Location</th>
-                    <th>Date</th>
-                    <th>Vin Number</th>
-                    <th>Make</th>
-                    <th>Model</th>
-                    <th>Due Date</th>
-                    <th>Status</th>
-                    <th>Image 1</th>
-                    <th>Image 2</th>
-                    <th>Image 3</th>
-                    <th>Image 4</th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php foreach ($result as $row) { ?>
-            <tr>
-                <td><?php echo escape($row["id"]); ?></td>
-                <td><?php echo escape($row["firstname"]); ?></td>
-                <td><?php echo escape($row["lastname"]); ?></td>
-                <td><?php echo escape($row["year"]); ?></td>
-                <td><?php echo escape($row["location"]); ?></td>
-                <td><?php echo escape($row["date"]); ?> </td>
-                <td><?php echo escape($row["vin"]); ?> </td>
-                <td><?php echo escape($row["make"]); ?> </td>
-                <td><?php echo escape($row["model"]); ?> </td>
-                <td><?php echo escape($row["due_date"]); ?> </td>
-                <td><?php echo escape($row["status"]); ?> </td>                
-              <td><a href="view.php?rand=<?php echo $row["image1"]; ?>"><img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
-              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
-            </tr>
-        <?php } ?>
-        </tbody>
-    </table>
+<?php require "templates/readtable.php"; ?>
+
     <?php } else { ?>
         <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
     <?php } 
@@ -108,50 +67,8 @@ if (isset($_POST['submit2'])) {
     if ($result && $statement->rowCount() > 0) { ?>
         <h2>Results</h2>
 
-               <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Year</th>
-                    <th>Location</th>
-                    <th>Date</th>
-                    <th>Vin Number</th>
-                    <th>Make</th>
-                    <th>Model</th>
-                    <th>Due Date</th>
-                    <th>Status</th>
-                    <th>Image 1</th>
-                    <th>Image 2</th>
-                    <th>Image 3</th>
-                    <th>Image 4</th>
-                    <th>View Details</th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php foreach ($result as $row) { ?>
-            <tr>
-                <td><?php echo escape($row["id"]); ?></td>
-                <td><?php echo escape($row["firstname"]); ?></td>
-                <td><?php echo escape($row["lastname"]); ?></td>
-                <td><?php echo escape($row["year"]); ?></td>
-                <td><?php echo escape($row["location"]); ?></td>
-                <td><?php echo escape($row["date"]); ?> </td>
-                <td><?php echo escape($row["vin"]); ?> </td>
-                <td><?php echo escape($row["make"]); ?> </td>
-                <td><?php echo escape($row["model"]); ?> </td>
-                <td><?php echo escape($row["due_date"]); ?> </td>
-                <td><?php echo escape($row["status"]); ?> </td>                
-              <td><a href="view.php?rand=<?php echo $row["image1"]; ?>"><img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
-              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
-            </tr>
-        <?php } ?>
-        </tbody>
-    </table>
+<?php require "templates/readtable.php"; ?>
+
     <?php } else { ?>
         <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
     <?php } 
@@ -186,49 +103,8 @@ if (isset($_POST['submit3'])) {
     if ($result && $statement->rowCount() > 0) { ?>
         <h2>Results</h2>
 
-               <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Year</th>
-                    <th>Location</th>
-                    <th>Date</th>
-                    <th>Vin Number</th>
-                    <th>Make</th>
-                    <th>Model</th>
-                    <th>Due Date</th>
-                    <th>Status</th>
-                    <th>Image 1</th>
-                    <th>Image 2</th>
-                    <th>Image 3</th>
-                    <th>Image 4</th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php foreach ($result as $row) { ?>
-            <tr>
-                <td><?php echo escape($row["id"]); ?></td>
-                <td><?php echo escape($row["firstname"]); ?></td>
-                <td><?php echo escape($row["lastname"]); ?></td>
-                <td><?php echo escape($row["year"]); ?></td>
-                <td><?php echo escape($row["location"]); ?></td>
-                <td><?php echo escape($row["date"]); ?> </td>
-                <td><?php echo escape($row["vin"]); ?> </td>
-                <td><?php echo escape($row["make"]); ?> </td>
-                <td><?php echo escape($row["model"]); ?> </td>
-                <td><?php echo escape($row["due_date"]); ?> </td>
-                <td><?php echo escape($row["status"]); ?> </td>                
-              <td><a href="view.php?rand=<?php echo $row["image1"]; ?>"><img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
-              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
-            </tr>
-        <?php } ?>
-        </tbody>
-    </table>
+<?php require "templates/readtable.php"; ?>
+
     <?php } else { ?>
         <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
     <?php } 
