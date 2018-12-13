b177949b89d8712fbc9bb6e3f654a0ef22e44c87
cochrand84 <david@davidcochran.us>
2018-12-12 23:11:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
+<link rel="stylesheet" href="public/css/style.css">
 <?php 
-include "public/templates/header.php";
+
 include "public/templates/password_protect.php"; 
 header("Location: public/index.php"); /* Redirect browser */
 exit();
