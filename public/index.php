4b7779b75117ac0d002f10e956868923329d0409
cochrand84 <david@davidcochran.us>
2018-12-12 20:55:13 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php include "templates/header.php"; ?>
 
 
-<ul>
-	<li><a href="create.php" class="linkbutton"><strong>Create</strong></a>add a new ticket</li>
-	<li><a href="read.php" class="linkbutton"><strong>Read</strong></a>show tickets</li>
-	<li><a href="edit.php" class="linkbutton"><strong>Edit</strong></a>edit tickets</li>
-</ul>
+
+<a href="create.php" class="linkbutton"><strong>Create</strong></a>add a new ticket</li>
+<a href="read.php" class="linkbutton"><strong>Read</strong></a>show tickets</li>
+<a href="edit.php" class="linkbutton"><strong>Edit</strong></a>edit tickets</li>
+
 
 <?php include "templates/footer.php"; ?>
