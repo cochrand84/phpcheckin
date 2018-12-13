8b3f00280d408d783abbbceca9cfba34d6484e2e
cochrand84 <david@davidcochran.us>
2018-12-12 22:20:26 -0600

    readtable.php created and added to read

--- public/read.php
+++ public/read.php
@@ -67,6 +67,7 @@ if (isset($_POST['submit'])) {
               <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
               <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
               <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
+              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
             </tr>
         <?php } ?>
         </tbody>
@@ -146,7 +147,7 @@ if (isset($_POST['submit2'])) {
               <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
               <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
               <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
-              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">Edit</a></td>
+              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
             </tr>
         <?php } ?>
         </tbody>
@@ -223,6 +224,7 @@ if (isset($_POST['submit3'])) {
               <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
               <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
               <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
+              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
             </tr>
         <?php } ?>
         </tbody>
---
+++ public/templates/readtable.php
@@ -0,0 +1,43 @@
+               <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Year</th>
+                    <th>Location</th>
+                    <th>Date</th>
+                    <th>Vin Number</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Due Date</th>
+                    <th>Status</th>
+                    <th>Image 1</th>
+                    <th>Image 2</th>
+                    <th>Image 3</th>
+                    <th>Image 4</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php foreach ($result as $row) { ?>
+            <tr>
+                <td><?php echo escape($row["id"]); ?></td>
+                <td><?php echo escape($row["firstname"]); ?></td>
+                <td><?php echo escape($row["lastname"]); ?></td>
+                <td><?php echo escape($row["year"]); ?></td>
+                <td><?php echo escape($row["location"]); ?></td>
+                <td><?php echo escape($row["date"]); ?> </td>
+                <td><?php echo escape($row["vin"]); ?> </td>
+                <td><?php echo escape($row["make"]); ?> </td>
+                <td><?php echo escape($row["model"]); ?> </td>
+                <td><?php echo escape($row["due_date"]); ?> </td>
+                <td><?php echo escape($row["status"]); ?> </td>                
+              <td><a href="view.php?rand=<?php echo $row["image1"]; ?>"><img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42"></a></td>
+              <td><a href="view.php?rand=<?php echo $row["image2"]; ?>"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
+              <td><a href="view.php?rand=<?php echo $row["image3"]; ?>"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
+              <td><a href="view.php?rand=<?php echo $row["image4"]; ?>"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
+              <td><a href="readdetails.php?editid=<?php echo $row["id"]; ?>">View</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
