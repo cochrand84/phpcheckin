846d031b219f72e7b66400afde82be1d7d2396d2
cochrand84 <david@davidcochran.us>
2018-12-12 18:40:10 -0600

    Update upload.php

--- public/upload.php
+++ public/upload.php
@@ -1,12 +1,12 @@
 <?php
-require "../config.php";
+
 
 if(isset($_POST["submit"])){
     $check = getimagesize($_FILES["image1"]["tmp_name"]);
     if($check !== false){
         $image1 = $_FILES['image1']['tmp_name'];
         $imgContent = addslashes(file_get_contents($image1));
-
+        require "../config.php";
         $db = new mysqli($host, $username, $password, $dbname);
        
         if($db->connect_error){
