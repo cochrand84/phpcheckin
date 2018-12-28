df84b219a6aa9dfe7f0b1c0dec18836fccf347b6
cochrand84 <david@davidcochran.us>
2018-12-27 18:19:46 -0600

    Update display.php

--- public/display.php
+++ public/display.php
@@ -42,7 +42,7 @@ require "templates/header.php";
 
         
 <?php  
-	echo $statement2;
+	
     if ($result && $statement->rowCount() > 0) { ?>
 <div class="displaytable">
                <table id="ticketstable" style="border-style:solid; border-width:2px; border-color: #c3d2d9;" cellspacing="0">
