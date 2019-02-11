f52cc230b1e6d793d6443a1733dc2f99f751b7f3
cochrand84 <david@davidcochran.us>
2019-02-11 12:19:56 -0600

    Update edit2.php

--- public/edit2.php
+++ public/edit2.php
@@ -263,6 +263,7 @@ if (isset($_POST['submitedit']) && $statement) { ?>
         </div>
         <div class="col-75">
             Oil Change/Service<br />
+            <?php echo $oilchangeckd; ?>
             <input type="checkbox" name="oilchange" id="oilchange" value="true" <?php echo $oilchangeckd; ?>/>Oil Change<br />
             <input type="checkbox" name="fullservice" id="fullservice" value="true"/>Full Service<br />
             <input type="checkbox" name="otherservice" id="otherservice" value="true"/>Other Service<br />
