b62543cf584d79c0c75322a4043d522153c33149
cochrand84 <david@davidcochran.us>
2019-02-11 17:15:02 -0600

    Update display4.php

--- public/display4.php
+++ public/display4.php
@@ -22,23 +22,9 @@ require "templates/header.php";
     }
 
     if ($result && $statement->rowCount() > 0) { ?>
-        <h2>Results</h2>
-
-               <table id="ticketstable">
-            <thead>
-                <tr>
-                    <th>Image 1</th>
-                </tr>
-            </thead>
-            <tbody>
         <?php foreach ($result as $row) { ?>
-            <tr>
-              <td><a href="view.php?rand=<?php echo $row["image1"]; ?>"><img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42"></a></td>
-            </tr>
-        <?php }  ?>
-        </tbody>
-    </table>
-
+<img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42">
+           <?php }  ?>
     <?php } else { ?>
         <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
     <?php } 
