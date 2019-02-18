6644dcbf433c881fcb00a38466702862d9ffe901
cochrand84 <david@davidcochran.us>
2019-02-18 11:58:11 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -18,7 +18,6 @@ include "templates/password_protect.php";
         </form>
 
 <a href="create.php" class="linkbutton"><strong>Create</strong></a></li>
-<a href="read.php" class="linkbutton"><strong>Read</strong></a></li>
 <a href="edit.php" class="linkbutton"><strong>Edit</strong></a></li>
 
 
