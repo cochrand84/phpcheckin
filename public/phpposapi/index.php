81a3df1db230356312b321d71f4838e0e3503a65
cochrand84 <david@davidcochran.us>
2019-12-11 17:53:09 -0600

    Update index.php

--- public/phpposapi/index.php
+++ public/phpposapi/index.php
@@ -1,4 +1,21 @@
 <?php
-$response = file_get_contents('https://demo.phppointofsale.com/index.php/api/v1/sales/1234');
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://demo.phppointofsale.com/index.php/api/v1/sales/1234",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
 echo $response;
+
 ?>
