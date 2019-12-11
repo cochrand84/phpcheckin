700c1bd580cfc88a12f0b2e1b34aedd0f455dfc6
cochrand84 <david@davidcochran.us>
2019-12-11 17:43:57 -0600

    Update index.php

--- public/phpposapi/index.php
+++ public/phpposapi/index.php
@@ -1,4 +1,4 @@
 <?php
-$response = file_get_contents('http://example.com/path/to/api/call?param1=5');
+$response = file_get_contents('"https://demo.phppointofsale.com/index.php/api/v1/sales/1234" -H "accept: application/json" -H "x-api-key: 123123123"');
 echo $response;
 ?>
