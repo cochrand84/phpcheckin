5156fe4f985bb386602c5b20cb317a09550c2924
cochrand84 <david@davidcochran.us>
2019-12-11 17:46:19 -0600

    Update index.php

--- public/phpposapi/index.php
+++ public/phpposapi/index.php
@@ -1,4 +1,4 @@
 <?php
-$response = file_get_contents('"https://demo.phppointofsale.com/index.php/api/v1/sales/1234" -H "accept: application/json" -H "x-api-key: 123123123"');
+$response = file_get_contents('https://demo.phppointofsale.com/index.php/api/v1/sales/1234');
 echo $response;
 ?>
