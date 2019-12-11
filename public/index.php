913224d7e8d27fb80b02eda90283ad7e0fc073b5
cochrand84 <david@davidcochran.us>
2019-12-11 16:15:34 -0600

    adding search

--- public/index.php
+++ public/index.php
@@ -16,7 +16,19 @@ include "templates/password_protect.php";
         <input type="submit" value="Submit">
     </div>
         </form>
-
+<form method="get" action="search.php"enctype="multipart/form-data">
+<div class="row">     
+        <div class="col-25">
+            <label for="searchvalue">Search:</label>
+        </div>
+        <div class="col-25">
+            <input type="text" name="searchvalue" id="searchvalue">
+        </div>
+    </div> 
+    <div class="row">
+        <input type="submit" value="Search">
+    </div>
+        </form>
 <a href="create.php" class="linkbutton"><strong>Create</strong></a></li>
 <a href="edit.php" class="linkbutton"><strong>Edit</strong></a></li>
 
---
+++ public/search.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Use an HTML form to create a new entry in the
+ * tickets table.
+ *
+ */
+
+require "templates/header.php";
+include "templates/password_protect.php"; 
+
+$_GET['searchvalue'];
+$incommingid = $_GET['searchvalue'];
+
+    try  {
+        
+        require "../config.php";
+        require "../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * FROM tickets WHERE '$incommingid' IN (coil1, coil2, lastname, color, date, phone, cellphone, firstname, city, address, id)";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+<?php require "templates/readtable.php"; ?>
+
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo $incommingid; ?>.</blockquote>
+    <?php } ?>
+
+<?php require "templates/footer.php"; ?>
