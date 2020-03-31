dd053bbe71568790c729153c21d757710b1dfc5b
cochrand84 <david@davidcochran.us>
2020-03-31 15:22:49 -0500

    Update edittable.php

--- public/templates/edittable.php
+++ public/templates/edittable.php
@@ -35,11 +35,7 @@
                 <td><?php echo escape($row["make"]); ?> </td>
                 <td><?php echo escape($row["model"]); ?> </td>
                 <td><?php echo escape($row["due_date"]); ?> </td>
-                <td><?php echo escape($row["status"]); ?> </td>                
-              <td><a href="view.php?rand=<?php echo $row["image1"]; ?>" data-lightbox="image-1"><img src="view.php?rand=<?php echo $row["image1"]; ?>" alt="<?php echo $row["image1"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image2"]; ?>" data-lightbox="image-1"><img src="view.php?rand=<?php echo $row["image2"]; ?>" alt="<?php echo $row["image2"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image3"]; ?>" data-lightbox="image-1"><img src="view.php?rand=<?php echo $row["image3"]; ?>" alt="<?php echo $row["image3"]; ?>" height="42" width="42"></a></td>
-              <td><a href="view.php?rand=<?php echo $row["image4"]; ?>" data-lightbox="image-1"><img src="view.php?rand=<?php echo $row["image4"]; ?>" alt="<?php echo $row["image4"]; ?>" height="42" width="42"></a></td>
+                <td><?php echo escape($row["status"]); ?> </td>        
               <td><a href="edit2.php?editid=<?php echo $row["id"]; ?>">Edit</a></td>
               <td><a href="complete.php?editid=<?php echo $row["id"]; ?>">Mark as Complete</a></td>
               <td><a href="print.php?editid=<?php echo $row["id"]; ?>">Print</a></td>
