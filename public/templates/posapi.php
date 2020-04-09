5d6c0d95e3dcac14e6a4c4d575b6fada83bc60ed
cochrand84 <david@davidcochran.us>
2020-04-09 18:36:00 -0500

    Update posapi.php

--- public/templates/posapi.php
+++ public/templates/posapi.php
@@ -23,13 +23,30 @@ $err = curl_error($curl);
 
 curl_close($curl);
 
-
-
-$dom = new DOMDocument;
-$dom->preserveWhiteSpace = FALSE;
-$dom->loadXML($response);
-$dom->formatOutput = TRUE;
-echo $dom->saveXML();
+function formatXmlString($response){
+    $xml = preg_replace('/(>)(<)(\/*)/', "$1\n$2$3", $xml);
+    $token      = strtok($xml, "\n");
+    $result     = '';
+    $pad        = 0; 
+    $matches    = array();
+    while ($token !== false) : 
+        if (preg_match('/.+<\/\w[^>]*>$/', $token, $matches)) : 
+          $indent=0;
+        elseif (preg_match('/^<\/\w/', $token, $matches)) :
+          $pad--;
+          $indent = 0;
+        elseif (preg_match('/^<\w[^>]*[^\/]>.*$/', $token, $matches)) :
+          $indent=1;
+        else :
+          $indent = 0; 
+        endif;
+        $line    = str_pad($token, strlen($token)+$pad, ' ', STR_PAD_LEFT);
+        $result .= $line . "\n";
+        $token   = strtok("\n");
+        $pad    += $indent;
+    endwhile; 
+    return $result;
+}
 
 //$decode = json_decode($response);
 
