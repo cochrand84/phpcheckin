648a88136f93e394eeccf1d5a1c9d07a3d8caacb
cochrand84 <david@davidcochran.us>
2020-04-09 19:39:21 -0500

    more print css

--- public/templates/posapi.php
+++ public/templates/posapi.php
@@ -27,7 +27,7 @@ echo '</br>';
 echo 'POS Ticket:';
 echo '</br>';
 echo '<table id="table-items">';
-echo '<tr>';
+echo '<tr class="items-row">';
 echo '<th>Quantity</th>';
 echo '<th>Product ID</th>';
 echo '<th>Description</th>';
@@ -36,7 +36,7 @@ echo '</tr>';
 
 $xml= new SimpleXMLElement($response) or die("Error: Cannot create object");
 foreach ($xml->cart_items->cart_item as $cart_item){
-  echo '<tr>','<td>',$cart_item->quantity,'</td>','<td>',$cart_item->product_id,'</td>','<td>',$cart_item->name,'</td>','</tr>';
+  echo '<tr class="items-row">','<td>',$cart_item->quantity,'</td>','<td>',$cart_item->product_id,'</td>','<td>',$cart_item->name,'</td>','</tr>';
 }
 
 echo '</table>';
