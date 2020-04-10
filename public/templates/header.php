0d34ea17602a99556d301ab396902cb36d4046fc
cochrand84 <david@davidcochran.us>
2020-04-10 16:47:51 -0500

    Update header.php

--- public/templates/header.php
+++ public/templates/header.php
@@ -23,3 +23,131 @@
 
 <body>
 	<h1>Area 51 Check-in App</h1>
+
+<a href="templates/password_protect.php?logout=1" class="linkbutton">Logout</a>
+<a href="index.php" class="linkbutton">Back to home</a>
+
+<?php
+
+/**
+ * Function to query information based on 
+ * a parameter: in this case, location.
+ *
+ */
+
+if (isset($_POST['submit'])) {
+    try  {
+        
+        require "../../config.php";
+        require "../../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * FROM tickets WHERE NOT (status = 'complete') ORDER by due_date ASC";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
+        
+<?php  
+if (isset($_POST['submit'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+<?php require "../templates/edittable.php"; ?>
+
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
+    <?php } 
+} ?>
+
+
+<?php
+
+if (isset($_POST['submit2'])) {
+    try  {
+        
+        require "../../config.php";
+        require "../../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * FROM tickets ORDER by due_date ASC";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+        
+<?php  
+if (isset($_POST['submit2'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+<?php require "../templates/edittable.php"; ?>
+
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
+    <?php } 
+} ?> 
+
+<?php
+
+if (isset($_POST['submit3'])) {
+    try  {
+        
+        require "../../config.php";
+        require "../../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * FROM tickets WHERE (status = 'complete') ORDER by due_date ASC";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+        
+<?php  
+if (isset($_POST['submit3'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+<?php require "../templates/edittable.php"; ?>
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
+    <?php } 
+} ?> 
+<h2>Select your edit option</h2>
+
+<form method="post">
+    <input type="submit" name="submit" value="Edit non-Complete">
+    <input type="submit" name="submit2" value="Edit All">
+    <input type="submit" name="submit3" value="Edit Complete">
+</form>
