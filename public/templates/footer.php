dc33e7b3fa8784704287b191cb7eaf0e6faa3b50
cochrand84 <david@davidcochran.us>
2020-04-13 09:43:49 -0500

    Update footer.php

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -5,6 +5,7 @@
 </br>
 </br>
 <a href="templates/password_protect.php?logout=1" class="linkbutton">Logout</a>
+<a href="create.php" class="linkbutton">Create New</a>
 <a href="index.php" class="linkbutton">Back to home</a>
 
 <script src="lightbox/js/lightbox-plus-jquery.js">
