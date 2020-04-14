e2ca0fd5bbc372e917c925a138c4cad1e8f0374f
cochrand84 <david@davidcochran.us>
2020-04-14 18:36:58 -0500

    Update mobile_edittable.php

--- public/templates/mobile_edittable.php
+++ public/templates/mobile_edittable.php
@@ -16,20 +16,20 @@
             <tbody>
         <?php foreach ($result as $row) { ?>
             <tr>
-                <label for="id">ID</label>
-                <td id="id"><?php echo escape($row["id"]); ?></td>
-                <label for="firstname">First Name</label>
-                <td id="firstname"><?php echo escape($row["firstname"]); ?></td>
-                <label for="lastname">Last Name</label>
-                <td id="lastname"><?php echo escape($row["lastname"]); ?></td>
-                <label for="location">Location</label>
-                <td id="location"><?php echo escape($row["location"]); ?></td>
-                <label for="due_date">Due Date</label>
-                <td id="due_date"><?php echo escape($row["due_date"]); ?> </td>
-                <label for="status">Status</label>
-                <td id="status"><?php echo escape($row["status"]); ?> </td>
-                <label for="tech">Tech</label>     
-                <td id="tech"><?php echo "NA"; ?> </td>   
+                <label for="id">ID
+                <td id="id"><?php echo escape($row["id"]); ?></td></label>
+                <label for="firstname">First Name
+                <td id="firstname"><?php echo escape($row["firstname"]); ?></td></label>
+                <label for="lastname">Last Name
+                <td id="lastname"><?php echo escape($row["lastname"]); ?></td></label>
+                <label for="location">Location
+                <td id="location"><?php echo escape($row["location"]); ?></td></label>
+                <label for="due_date">Due Date
+                <td id="due_date"><?php echo escape($row["due_date"]); ?> </td></label>
+                <label for="status">Status
+                <td id="status"><?php echo escape($row["status"]); ?> </td></label>
+                <label for="tech">Tech    
+                <td id="tech"><?php echo "NA"; ?> </td>  </label> 
               <td><a href="edit2.php?editid=<?php echo $row["id"]; ?>" class="linkbutton">Edit</a></td>
               <td><a href="complete.php?editid=<?php echo $row["id"]; ?>" class="linkbutton">Mark as Complete</a></td>
               <td><a href="checkout.php?editid=<?php echo $row["id"]; ?>" class="linkbutton">Check-out</a></td>
