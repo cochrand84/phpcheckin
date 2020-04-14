80d60210203cd46e432d966d4f9a101c4a126f29
cochrand84 <david@davidcochran.us>
2020-04-14 18:49:45 -0500

    Update edit.php

--- public/edit.php
+++ public/edit.php
@@ -12,137 +12,164 @@ $incommingid = $_GET['linkid'];
 
 $detect = new Mobile_Detect;
 
-if ( $detect->isMobile() ) {
- echo '<link rel="stylesheet" href="css/mobile_table_style.css">';
-}
-else{
+if ($detect->isMobile()) {
+    echo '<link rel="stylesheet" href="css/mobile_table_style.css">';
+} else {
     echo '<link rel="stylesheet" href="css/table_style.css">';
 }
 
 if ($incommingid == 'noncomplete') {
-    try  {
+    try {
         
         require "../config.php";
         require "../common.php";
-
+        
         $connection = new PDO($dsn, $username, $password, $options);
-
+        
         $sql = "SELECT * FROM tickets WHERE NOT (status = 'complete') ORDER by due_date ASC";
-
+        
         $location = $_POST['location'];
-
+        
         $statement = $connection->prepare($sql);
         $statement->bindParam(':location', $location, PDO::PARAM_STR);
         $statement->execute();
-
+        
         $result = $statement->fetchAll();
-    } catch(PDOException $error) {
+    }
+    catch (PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
 }
 ?>
 
         
-<?php  
+<?php
 if ($incommingid == 'noncomplete') {
-    if ($result && $statement->rowCount() > 0) { ?>
-        <h2>Results</h2>
+    if ($result && $statement->rowCount() > 0) {
+?>
+       <h2>Results</h2>
 
-<?php 
-if ( $detect->isMobile() ) {
- require "templates/mobile_edittable.php";
+<?php
+        if ($detect->isMobile()) {
+            require "templates/mobile_edittable.php";
+        } else {
+            require "templates/edittable.php";
+        }
+?>
+
+    <?php
+    } else {
+?>
+       <blockquote>No results found for <?php
+        echo escape($_POST['status']);
+?>.</blockquote>
+    <?php
+    }
 }
-else{
-    require "templates/edittable.php";
-} ?>
-
-    <?php } else { ?>
-        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
-    <?php } 
-} ?>
+?>
 
 
 <?php
 
 if ($incommingid == 'all') {
-    try  {
+    try {
         
         require "../config.php";
         require "../common.php";
-
+        
         $connection = new PDO($dsn, $username, $password, $options);
-
+        
         $sql = "SELECT * FROM tickets ORDER by due_date ASC";
-
+        
         $location = $_POST['location'];
-
+        
         $statement = $connection->prepare($sql);
         $statement->bindParam(':location', $location, PDO::PARAM_STR);
         $statement->execute();
-
+        
         $result = $statement->fetchAll();
-    } catch(PDOException $error) {
+    }
+    catch (PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
 }
 ?>
-        
-<?php  
+       
+<?php
 if ($incommingid == 'all') {
-    if ($result && $statement->rowCount() > 0) { ?>
-        <h2>Results</h2>
+    if ($result && $statement->rowCount() > 0) {
+?>
+       <h2>Results</h2>
 
-<?php if ( $detect->isMobile() ) {
- require "templates/mobile_edittable.php";
+<?php
+        if ($detect->isMobile()) {
+            require "templates/mobile_edittable.php";
+        } else {
+            require "templates/edittable.php";
+        }
+?>
+
+    <?php
+    } else {
+?>
+       <blockquote>No results found for <?php
+        echo escape($_POST['status']);
+?>.</blockquote>
+    <?php
+    }
 }
-else{
-    require "templates/edittable.php";
-} ?>
-
-    <?php } else { ?>
-        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
-    <?php } 
-} ?> 
+?> 
 
 <?php
 
 if ($incommingid == 'complete') {
-    try  {
+    try {
         
         require "../config.php";
         require "../common.php";
-
+        
         $connection = new PDO($dsn, $username, $password, $options);
-
+        
         $sql = "SELECT * FROM tickets WHERE (status = 'complete') ORDER by due_date ASC";
-
+        
         $location = $_POST['location'];
-
+        
         $statement = $connection->prepare($sql);
         $statement->bindParam(':location', $location, PDO::PARAM_STR);
         $statement->execute();
-
+        
         $result = $statement->fetchAll();
-    } catch(PDOException $error) {
+    }
+    catch (PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
 }
 ?>
-        
-<?php  
+       
+<?php
 if ($incommingid == 'complete') {
-    if ($result && $statement->rowCount() > 0) { ?>
-        <h2>Results</h2>
+    if ($result && $statement->rowCount() > 0) {
+?>
+       <h2>Results</h2>
 
-<?php if ( $detect->isMobile() ) {
- require "templates/mobile_edittable.php";
+<?php
+        if ($detect->isMobile()) {
+            require "templates/mobile_edittable.php";
+        } else {
+            require "templates/edittable.php";
+        }
+?>
+   <?php
+    } else {
+?>
+       <blockquote>No results found for <?php
+        echo escape($_POST['status']);
+?>.</blockquote>
+    <?php
+    }
 }
-else{
-    require "templates/edittable.php";
-} ?>
-    <?php } else { ?>
-        <blockquote>No results found for <?php echo escape($_POST['status']); ?>.</blockquote>
-    <?php } 
-} ?> 
+?> 
 
-<?php require "templates/footer.php"; ?>
+<?php
+require "templates/footer.php";
+?>
