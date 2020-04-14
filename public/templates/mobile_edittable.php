b2e9a9064bae5a798f86c19739dfa43e61a6b897
cochrand84 <david@davidcochran.us>
2020-04-14 18:41:38 -0500

    Update mobile_edittable.php

--- public/templates/mobile_edittable.php
+++ public/templates/mobile_edittable.php
@@ -16,20 +16,13 @@
             <tbody>
         <?php foreach ($result as $row) { ?>
             <tr>
-                <label for="id">ID
-                <td id="id"><?php echo escape($row["id"]); ?></td></label>
-                <label for="firstname">First Name
-                <td id="firstname"><?php echo escape($row["firstname"]); ?></td></label>
-                <label for="lastname">Last Name
-                <td id="lastname"><?php echo escape($row["lastname"]); ?></td></label>
-                <label for="location">Location
-                <td id="location"><?php echo escape($row["location"]); ?></td></label>
-                <label for="due_date">Due Date
-                <td id="due_date"><?php echo escape($row["due_date"]); ?> </td></label>
-                <label for="status">Status
-                <td id="status"><?php echo escape($row["status"]); ?> </td></label>
-                <label for="tech">Tech    
-                <td id="tech"><?php echo "NA"; ?> </td>  </label> 
+                <td><?php echo escape($row["id"]); ?></td>
+                <td><?php echo escape($row["firstname"]); ?></td>
+                <td><?php echo escape($row["lastname"]); ?></td>
+                <td><?php echo escape($row["location"]); ?></td>
+                <td><?php echo escape($row["due_date"]); ?> </td>
+                <td><?php echo escape($row["status"]); ?> </td>     
+                <td><?php echo "NA"; ?> </td>   
               <td><a href="edit2.php?editid=<?php echo $row["id"]; ?>" class="linkbutton">Edit</a></td>
               <td><a href="complete.php?editid=<?php echo $row["id"]; ?>" class="linkbutton">Mark as Complete</a></td>
               <td><a href="checkout.php?editid=<?php echo $row["id"]; ?>" class="linkbutton">Check-out</a></td>
