1da2f2769a5a9b40ee3d86cb2002454c62fea959
cochrand84 <david@davidcochran.us>
2020-05-29 19:51:57 -0500

    Update tracking.php

--- public/templates/tracking.php
+++ public/templates/tracking.php
@@ -1,19 +1,11 @@
 
-<?php
-
-$tracking_username = "null";
-
-$tracking_username = $_SESSION["username"];
-
-echo $tracking_username;
-?>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163409638-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('set', {'user_id': '<?php $tracking_username ?>'}); // Set the user ID using signed-in user_id.
+  gtag('set', {'user_id': '<%= Session["username"] %>'}); // Set the user ID using signed-in user_id.
   gtag('config', 'UA-163409638-1');
 
 
