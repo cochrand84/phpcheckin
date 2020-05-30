cab296edc47e0c9752fd5ddca722de0fbc55c679
cochrand84 <david@davidcochran.us>
2020-05-29 19:10:33 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,21 @@
 <link rel="stylesheet" href="css/style.css">
 <?php 
 
-include "templates/header.php"; 
+include // "templates/header.php"; 
+
+
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
 
 ?>
-<form method="get" action="search.php"enctype="multipart/form-data">
+<!--<form method="get" action="search.php"enctype="multipart/form-data">
 <div class="row">     
         <div class="col-25">
             <label for="searchvalue">Search:</label>
@@ -18,4 +29,6 @@ include "templates/header.php";
     </div>
         </form>
 
-<?php include "templates/footer.php"; ?>
+<?php // include "templates/footer.php"; ?>
+
+-->
