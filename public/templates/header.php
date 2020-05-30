9c7972a1253351cbda69130a1fd0f94a63e848e5
cochrand84 <david@davidcochran.us>
2020-05-29 19:14:15 -0500

    Update header.php

--- public/templates/header.php
+++ public/templates/header.php
@@ -17,10 +17,10 @@
 include "tracking.php";
 
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
+// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//     header("location: login.php");
+//     exit;
+// }
 
 ?>
 
