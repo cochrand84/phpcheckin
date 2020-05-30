eec897982c6ee03374dca7802d628634dd988456
cochrand84 <david@davidcochran.us>
2020-05-29 19:13:33 -0500

    fixing the header

--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <link rel="stylesheet" href="css/style.css">
 <?php 
 
-// include "templates/header.php"; 
+include "templates/header.php"; 
 
 
 // Initialize the session
 session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
-
 
 ?>
 <!--<form method="get" action="search.php"enctype="multipart/form-data">
--- public/templates/header.php
+++ public/templates/header.php
@@ -16,6 +16,7 @@
 
 include "tracking.php";
 
+// Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
