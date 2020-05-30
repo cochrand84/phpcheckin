aa0457bae18ca4b727b416cb80c4cfce183c6750
cochrand84 <david@davidcochran.us>
2020-05-29 19:20:18 -0500

    Update tracking.php

--- public/templates/tracking.php
+++ public/templates/tracking.php
@@ -1,4 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
+<!--
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163409638-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -6,4 +7,17 @@
   gtag('js', new Date());
 
   gtag('config', 'UA-163409638-1');
+
+
 </script>
+-->
+<script>
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-163409638-1', 'auto');
+ ga('set', 'userId', '<?php $username ?>');
+ ga('set', 'dimension1', 'Author');
+ ga('send', 'pageview');
+ </script>
