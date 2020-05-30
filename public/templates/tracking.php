83bba17e03f47a3f1e6e831e2325d48e56e160e7
cochrand84 <david@davidcochran.us>
2020-05-29 19:27:36 -0500

    Update tracking.php

--- public/templates/tracking.php
+++ public/templates/tracking.php
@@ -1,23 +1,18 @@
+
+<?php
+$_SESSION["username"] = $tracking_username;
+?>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<!--
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163409638-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
+  gtag('set', {'user_id': '<?php $tracking_username ?>'}); // Set the user ID using signed-in user_id.
   gtag('config', 'UA-163409638-1');
 
 
 </script>
--->
-<script>
- (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
- (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-163409638-1', 'auto');
- ga('set', 'userId', '<?php $username ?>');
- ga('set', 'dimension1', 'Author');
- ga('send', 'pageview');
- </script>
+
+
+
