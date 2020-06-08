33a0d98ddad05cbb61625f262810cbdba6d7dd77
cochrand84 <david@davidcochran.us>
2020-06-08 18:07:29 -0500

    Update header.php

--- public/templates/header.php
+++ public/templates/header.php
@@ -16,11 +16,11 @@
 
 include "tracking.php";
 
-// Check if the user is logged in, if not then redirect him to login page
-// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-//     header("location: login.php");
-//     exit;
-// }
+//Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: http://checkin.area51custom.com/public/index.php");
+     exit;
+ }
 
 ?>
 
