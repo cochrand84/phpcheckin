13d52490bf27b5e9f2e61f4909a02d8f92b8f482
David Cochran <david@davidcochran.us>
2021-05-06 22:06:08 -0500

    Create customersapi.php

---
+++ public/templates/customersapi.php
@@ -0,0 +1,26 @@
+<?php
+$curl = curl_init();
+
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://area51customs.phppointofsale.com/index.php/api/v1/customers",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HEADER => false,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+  	"accept: application/xml",
+    "cache-control: no-cache",
+    "x-api-key: s088wogkssw84cwwkgggk4w040coowggg4c08c44",
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+echo $response;
+
+?>
