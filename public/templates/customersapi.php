0f45055d3e8fd00833eb8c7754ec7f1121b6c34b
David Cochran <david@davidcochran.us>
2021-05-06 22:38:45 -0500

    Update customersapi.php

--- public/templates/customersapi.php
+++ public/templates/customersapi.php
@@ -3,19 +3,19 @@
 
 
 function searchapi(){
-  
+
 $search = $_POST['serachvalue'];
 $curl = curl_init();
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://area51customs.phppointofsale.com/index.php/api/v1/customers?search='$search'&search_field=last_name",
+  CURLOPT_URL => "https://area51customs.phppointofsale.com/index.php/api/v1/customers?search="$search"&search_field=last_name",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_HEADER => false,
-  CURLOPT_TIMEOUT => 900,
+  CURLOPT_TIMEOUT => 60,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
   	"accept: application/json",
-    "cache-control: must-revalidate",
+    "cache-control: no-cache",
     "x-api-key: s088wogkssw84cwwkgggk4w040coowggg4c08c44",
   ),
 ));
@@ -26,6 +26,7 @@ $err = curl_error($curl);
 curl_close($curl);
 
 echo $response;
+
 }
 
 if (isset($_POST['submit'])) {
