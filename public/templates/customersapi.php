ad9b90260c361f98a99bf46c9ff970091cc2fed5
David Cochran <david@davidcochran.us>
2021-05-07 17:44:41 -0500

    Update customersapi.php

--- public/templates/customersapi.php
+++ public/templates/customersapi.php
@@ -29,6 +29,38 @@ echo $response;
 
 }
 
+function searchapi2(){
+
+$search = $_POST['searchvalue2'];
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://area51customs.phppointofsale.com/index.php/api/v1/sales/' . $search,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HEADER => false,
+  CURLOPT_TIMEOUT => 60,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "accept: application/json",
+    "cache-control: no-cache",
+    "x-api-key: s088wogkssw84cwwkgggk4w040coowggg4c08c44",
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+echo $response;
+
+}
+if (isset($_POST['submit2'])) {
+
+searchapi2();
+
+}
+
 if (isset($_POST['submit'])) {
 
 searchapi();
@@ -38,7 +70,7 @@ searchapi();
 <form method="post" enctype="multipart/form-data">
 <div class="row">     
         <div class="col-25">
-            <label for="searchvalue">Search:</label>
+            <label for="searchvalue">Search for customer:</label>
         </div>
         <div class="col-25">
             <input type="text" name="searchvalue" id="searchvalue">
@@ -48,5 +80,19 @@ searchapi();
     </div>
 
     <input type="submit" name="submit" value="Search">
+</p>
+    <div class="row">     
+        <div class="col-25">
+            <label for="searchvalue2">Search for POs id:</label>
+        </div>
+        <div class="col-25">
+            <input type="text" name="searchvalue2" id="searchvalue2">
+        </div>
+    </div> 
+    <div class="row">
+    </div>
+
+    <input type="submit" name="submit2" value="Search">
+
 
 </form>
